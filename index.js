4e1c90c6498176c56a5f5221cb7bffe277d78bfa
AlvaradoHarlam-FS <hjalvarado@student.fullsail.edu>
2022-12-11 21:58:01 -0500

    complete week3

--- index.js
+++ index.js
@@ -4,10 +4,14 @@ const app = require("liquid-express-views")(express())
 app.use(express.urlencoded({ extended: false }))
 
 const productRouter = require("./routes/Products")
+const variantRouter = require("./routes/Variants")
+const imageRouter = require("./routes/Images")
 app.use("/products", productRouter)
+app.use("/variants", variantRouter)
+app.use("/images", imageRouter)
 
 app.listen(3000, () => {
-    console.log(' Server listening on port 3000')
+    console.log(' Server running on port 3000')
 })
 //Week 1
 // index =http://localhost:3000/products 
